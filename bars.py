b632c173889e4a8175cd72e891153832fd9ff802
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2018-07-04 15:01:20 +0300

    Fixed incorrect dict requestse

--- bars.py
+++ bars.py
@@ -18,28 +18,28 @@ def fetch_bars_data():
 
 
 def get_biggest_bar(bars_data):
-    big_bar_data = max(bars_data,
-                       key = lambda bars: bars.get('SeatsCount'))
-    big_bar_data_short = {'Name':big_bar_data['Name'],
-                          'Address':big_bar_data['Address'],
-                          'SeatsCount':big_bar_data['SeatsCount']}
-    return big_bar_data_short
+    big_data = max(bars_data,
+                    key = lambda bars: bars['Cells']['SeatsCount'])
+    big_data_short = {'Name':big_data['Cells']['Name'],
+                      'Address':big_data['Cells']['Address'],
+                      'SeatsCount':big_data['Cells']['SeatsCount']}
+    return big_data_short
 
 
 def get_smallest_bar(bars_data):
-    small_bar_data = max(bars_data,
-                         key = lambda bars: bars.get('SeatsCount'))
-    small_bar_data_short = {'Name':small_bar_data['Name'],
-                            'Address':small_bar_data['Address'],
-                            'SeatsCount':small_bar_data['SeatsCount']}
-    return small_bar_data_short
+    small_data = max(bars_data,
+                     key = lambda bars: bars['Cells']['SeatsCount'])
+    small_data_short = {'Name':small_data['Cells']['Name'],
+                        'Address':small_data['Cells']['Address'],
+                        'SeatsCount':small_data['Cells']['SeatsCount']}
+    return small_data_short
 
 
 def get_closest_bar(bars_data, latitude, longitude):
     x_user, y_user = latitude, longitude
     for bar_data in bars_data:
-        x_bar = float(bar_data['geoData']['coordinates'][0])
-        y_bar = float(bar_datas['geoData']['coordinates'][1])
+        x_bar = float(bar_data['Cells']['geoData']['coordinates'][0])
+        y_bar = float(bar_datas['Cells']['geoData']['coordinates'][1])
         distance = sqrt((x_bar - x_user)**2 + (y_bar - y_user)**2)
         bar_data['user_distance'] = distance
     closest_bar_data = min(bars_data, 
@@ -50,11 +50,12 @@ def get_closest_bar(bars_data, latitude, longitude):
     return closest_bar_data_short
 
 
+def main():
+    fetched_data = fetch_bars_data()
+    biggest_bar_info = get_biggest_bar(fetched_data)
+    smallest_bar_info = get_smallest_bar(fetched_data)
+    print(biggest_bar_info)
+
 
 if __name__ == '__main__':
-    result = fetch_bars_data()
-    #print(get_biggest_bar(fetched_data))
-    print(type(result))
-    print(result[0])
-    print(type(result[0]))
-    #print(result)
+    main()
